07b497df3410f788511b5779e129c6bf28c05ded
William Vann <billthelibrarian@gmail.com>
2022-07-12 14:45:40 -0500

    Solved PyBank delta computations!

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,10 @@
-# William Vann, Homework #3 PyBank
+# William Vann, Homework #3 PyBank, Submission #2 (got the delta average figured out!)
 
 import csv
 import os
-
-    # Relative filepath for csv file 
-
+    
 csvpath = os.path.join("Resources", "budget_data.csv")
+csvdata = []
 
 with open(csvpath, "r", encoding="utf") as csvfile:
     
@@ -13,83 +12,63 @@ with open(csvpath, "r", encoding="utf") as csvfile:
 
     csvheader = next(csvfile)
 
-        # <<<Header test>>>:
-        # print(csvheader) 
-
+        # create csvreader object
+            
     csvreader = csv.reader(csvfile, delimiter=",")
 
-        # Initialize list for dates values, list for earnings values
+    for row in csvreader:
+        csvdata.append([row[0], int(row[1])]) # add month-yr str and profit/loss int to list
+       
+    new_csv_list = []
+    delta = None
+        
+    for i,list in enumerate(csvdata):   # iterate over list, compute delta value, append to new list
+        
+        if i==0:
+            delta = None
+        else:
+            try:
+                delta = int(csvdata[i][1]) - int(csvdata[i-1][1]) 
+            except:
+                delta = None
+       
+        new_csv_list.append([i, list[0], list[1], delta])  # index number, date/yr, profit/loss, delta value
 
-    dates_list = []
+    delta_list = []
+
+    total_months = 0
+    total_months = len(new_csv_list)
+
     earnings_list = []
 
-        # Iterate over rows in csvreader object
+    for list in new_csv_list:       # iterate over all lists and populate delta_list and earnings_list for computations
+        
+        if list[3]:       
+            delta_list.append(list[3])         
 
-    for row in csvreader:
+        if list[2]:
+            earnings_list.append(int(list[2]))
 
-            # Append values to their respective lists, casting ints for earnings values
-
-        dates_list.append(row[0])
-        earnings_list.append(int(row[1]))
-    
-        # Run calculations and fetch dates
-
-    total_months = len(dates_list)
+    total_earnings = 0 
     total_earnings = sum(earnings_list)
 
-    greatest_increase_profits = max(earnings_list)
-    greatest_decrease_profits = min(earnings_list)
+    greatest_increase_profits = max(delta_list)
+    greatest_decrease_profits = min(delta_list)
 
-    greatest_increase_profits_date = dates_list[earnings_list.index(greatest_increase_profits)]
-    greatest_decrease_profits_date = dates_list[earnings_list.index(greatest_decrease_profits)] 
-    
-        # Initialize list for moving averages
-
-    moving_averages_list = []
-
-        # Make sure we have correct start and end earning values 
-
-    start = earnings_list[0]
-    end = earnings_list[len(earnings_list)-1]
-
-
-        # <<<Start / End Test>>>: 
-    # print(f"Start: {start}")
-    # print(f"End: {end}")
-        
-        # Set moving average window size and initialize counter
-
-    window_size = 3
-    i = 0
-    
-        # Iterate through all 3 pane "windows" in earnings list
-        # Calculate moving average for each window
-        # Write moving average to moving averages list
-        # (NB: I found the following logic here: 
-        # https://www.geeksforgeeks.org/how-to-calculate-moving-averages-in-python/)
-
-    while i < (len(earnings_list) - window_size + 1): 
-
-        window = earnings_list[i : i + window_size]
-        average = round(sum(window) / window_size, 2)
-        moving_averages_list.append(average)
-        i += 1
-    
-        # Calculate average change of moving averages
-    
-    # print(moving_averages_list)
-
-    average_change = round(sum(moving_averages_list) / len(moving_averages_list), 2)
-    
-        # Print summary results to terminal
+    greatest_increase_profits_date = new_csv_list[delta_list.index(greatest_increase_profits)+1][1]     # add 1 to delta_list index for 
+    greatest_decrease_profits_date = new_csv_list[delta_list.index(greatest_decrease_profits)+1][1]     #   index in new_csv_list where date/yr
+                                                                                                        #   can be retrieved
+    average_change = round(sum(delta_list) / len(delta_list), 2)                                        # average of all the delta values
+       
+       # Print summary results to terminal
 
     print(f"Financial Analysis")
     print(f"----------------------------")
-    print(f"Total Months: {len(dates_list)}")
-    print(f"Total: ${sum(earnings_list)}")
+    print(f"Total Months: {total_months}")
+    print(f"Total: ${total_earnings}")
     print(f"Average Change: ${average_change}")
-    print(f"Greatest Increase in Profits: {greatest_increase_profits_date} (${max(earnings_list)})")
-    print(f"Greatest Decrease in Profits: {greatest_decrease_profits_date} (${min(earnings_list)})")
+    print(f"Greatest Increase in Profits: {greatest_increase_profits_date} (${greatest_increase_profits})")
+    print(f"Greatest Decrease in Profits: {greatest_decrease_profits_date} (${greatest_decrease_profits})")
     
         # Print summary results to txt file
 
@@ -101,48 +80,11 @@ with open(csvpath, "r", encoding="utf") as csvfile:
 
         txtfile.write(f"Financial Analysis\n")
         txtfile.write(f"----------------------------\n")
-        txtfile.write(f"Total Months: {len(dates_list)}\n")
-        txtfile.write(f"Total: ${sum(earnings_list)}\n")
+        txtfile.write(f"Total Months: {total_months}\n")
+        txtfile.write(f"Total: ${total_earnings}\n")
         txtfile.write(f"Average Change: ${average_change}\n")
-        txtfile.write(f"Greatest Increase in Profits: {greatest_increase_profits_date} (${max(earnings_list)})\n")
-        txtfile.write(f"Greatest Decrease in Profits: {greatest_decrease_profits_date} (${min(earnings_list)})\n")
+        txtfile.write(f"Greatest Increase in Profits: {greatest_increase_profits_date} (${greatest_increase_profits})\n")
+        txtfile.write(f"Greatest Decrease in Profits: {greatest_decrease_profits_date} (${greatest_decrease_profits})\n")
 
 
 
-#####################################################################
-######################## DELIVERABLE EXAMPLE ########################
-#####################################################################
-
-'''
-
-Financial Analysis
-----------------------------
-Total Months: 86
-Total: $22564198
-Average Change: $-8311.11
-Greatest Increase in Profits: Aug-16 ($1862002)
-Greatest Decrease in Profits: Feb-14 ($-1825558)
-
-'''
-
-#####################################################################
-######################## DELIVERABLE TEMPLATE #######################
-#####################################################################
-
-'''
-
-f"Financial Analysis"
-f"----------------------------"
-f"Total Months: {total_months}"       # get length of months_list
-f"Total: $ {total_earnings}"          # get sum of earnings_list
-f"Average Change: $ {average_change}" # average of moving average list elements with "window" size = 3
-
-    # get max of earnings list and fetch corresponding date 
-
-f"Greatest Increase in Profits: {greatest_increase_profits_date} (${greatest_increase_profits})"  
-
-    # get min of earnings list and fetch corresponding date 
-    
-f"Greatest Decrease in Profits: {greatest_decrease_profits_date} (${greatest_decrease_profits})"  
-
-'''
